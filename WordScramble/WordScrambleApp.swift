ff4bae993b8271e2118ca6383e095882232f7f72
Samuel Ebuka <samiebuka@impronta.es>
2022-01-01 09:59:38 +0100

    Initial Commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Samuel Ebuka on 1/1/22.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
